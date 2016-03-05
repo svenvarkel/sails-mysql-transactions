6529364dd1a83da4436ff37e4d2eba46ad2f2aa5
Sven Varkel <sven@mageflow.com>
2016-03-05 23:29:54 +0200

    removed sudo from postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -34,7 +34,7 @@ fi
 
 echo "${INFO} Injecting waterline...";
 pushd "${MOD_DIR}/sails" > /dev/null;
-sudo npm remove waterline;
+npm remove waterline;
 npm install "${MOD_DIR}/sails-mysql-transactions/waterline";
 popd > /dev/null;
 
